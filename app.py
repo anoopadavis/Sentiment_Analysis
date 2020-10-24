a9643febef4bb3dbc4b5b7dc1b7cffecea4d59c3
anoopadavis <70140032+anoopadavis@users.noreply.github.com>
2020-10-24 15:29:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from joblib import load
+import pandas as pd
+from PIL import Image
+from sklearn.feature_extraction.text import CountVectorizer
+
+clf = load('filename.joblib')
+
+cv = CountVectorizer()
+st.title("SENTIMENT ANALYSIS")
+image = Image.open('IMG-20200904-WA0251.jpg')
+st.image(image, width=800)
+review = st.text_input('Enter your short review :')
+df = {'review': review}
+df = pd.DataFrame(df, index=[0])
+to_pred = df.iloc[:, 0]
+result = clf.predict(cv.transform(to_pred))
+if(st.button('Predict')):
+    st.write(result[0])
